b70527d6786143ad7f4f3d4574b621de837e7a22
elijacode <pakalnis.elijas@gmail.com>
2019-07-12 05:13:53 +0100

    add dependency for logback and implement it in code

--- src/main/java/com/github/elijacode/HelloMaven.java
+++ src/main/java/com/github/elijacode/HelloMaven.java
@@ -1,10 +1,13 @@
 package com.github.elijacode;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class HelloMaven {
+  private static final Logger log = LoggerFactory.getLogger(HelloMaven.class);
+
   public static void main(String[] args) {
-
-    System.out.println("Hello Maven!");
-    System.out.println("Kay got the basics of maven and its lifecycle!");
-
+    log.info("Info Log");
+    log.debug("Debug Log");
   }
 }
